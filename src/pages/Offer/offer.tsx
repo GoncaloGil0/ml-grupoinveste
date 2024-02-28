06626a86bfabfe850aa7a851bd9547d863c82e63
Gonçalo Gil <goncalogil@notcybersec.com>
2024-02-28 16:22:27 +0000

    Update offer.tsx

--- src/pages/Offer/offer.tsx
+++ src/pages/Offer/offer.tsx
@@ -23,7 +23,7 @@ const Offer: React.FC = () => {
     gamming: useRef(null),
   };
 
-  const [visibleSection, setVisibleSection] = useState<string | null>(null);
+ const [visibleSection, setVisibleSection] = useState<string | null>(null);
 /*
   useEffect(() => {
     const checkVisibility = () => {
@@ -71,27 +71,27 @@ const Offer: React.FC = () => {
         <Hero.Description desc={t('offer.hero.subTitle')} />
       </Hero.Root>
       <div className='array'>
-        <section className={visibleSection === 'consulting' ? 'visible' : ''} id='consulting' ref={sectionRefs.consulting}>
+        <section id='consulting' ref={sectionRefs.consulting}>
           <h1>{t('offer.section1.title')} <span style={{ borderColor: `${"#000"}` }} /></h1>
           <p>{t('offer.section1.text1')}</p>
           <p>{t('offer.section1.text2')}</p>
         </section>
-        <section className={visibleSection === 'cybersecurity' ? 'visible' : ''} id='cybersecurity' ref={sectionRefs.cybersecurity}>
+        <section id='cybersecurity' ref={sectionRefs.cybersecurity}>
           <h1>{t('offer.section2.title')} <span style={{ borderColor: `${"#000"}` }} /></h1>
           <p>{t('offer.section2.text1')}</p>
           <p>{t('offer.section2.text2')}</p>
         </section>
-        <section className={visibleSection === 'networking' ? 'visible' : ''} id='networking' ref={sectionRefs.networking}>
+        <section id='networking' ref={sectionRefs.networking}>
           <h1>{t('offer.section3.title')} <span style={{ borderColor: `${"#000"}` }} /></h1>
           <p>{t('offer.section3.text1')}</p>
           <p>{t('offer.section3.text2')}</p>
         </section>
-        <section className={visibleSection === 'marketing' ? 'visible' : ''} id='marketing' ref={sectionRefs.marketing}>
+        <section id='marketing' ref={sectionRefs.marketing}>
           <h1>{t('offer.section4.title')} <span style={{ borderColor: `${"#000"}` }} /></h1>
           <p>{t('offer.section4.text1')}</p>
           <p>{t('offer.section4.text2')}</p>
         </section>
-        <section className={visibleSection === 'gamming' ? 'visible' : ''} id='gaming' ref={sectionRefs.gamming}>
+        <section id='gaming' ref={sectionRefs.gamming}>
           <h1>{t('offer.section5.title')} <span style={{ borderColor: `${"#000"}` }} /></h1>
           <p>{t('offer.section5.text1')}</p>
           <p>{t('offer.section5.text2')}</p>
