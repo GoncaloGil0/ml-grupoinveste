f8e1d1e63cd70f0b753f9e5452ca7daeea8aa34a
Gonçalo Gil <goncalogil@rootkey.ai>
2024-11-07 17:02:38 +0000

    Update rh.tsx

--- src/data/rh.tsx
+++ src/data/rh.tsx
@@ -15,10 +15,6 @@ export const rh = [{
   "link": "https://media.grupoinveste.pt/gp/person/goncalo_gil.jpg"
 },
 {
-  "nome": "Henrique Esteves",
-  "link": "https://media.grupoinveste.pt/gp/person/Henrique_Esteves.jpg"
-},
-{
   "nome": "Hugo Ré",
   "link": "https://media.grupoinveste.pt/gp/person/Hugo_Re.jpg"
 },
@@ -31,10 +27,6 @@ export const rh = [{
   "link": "https://media.grupoinveste.pt/gp/person/Liliana_Batista.jpg"
 },
 {
-  "nome": "Matilde Costa",
-  "link": "https://media.grupoinveste.pt/gp/person/Matilde_Costa.jpg"
-},
-{
   "nome": "Miguel Gonçalves",
   "link": "https://media.grupoinveste.pt/gp/person/Miguel_Goncalves.jpg"
 },
@@ -43,19 +35,7 @@ export const rh = [{
   "link": "https://media.grupoinveste.pt/gp/person/Pedro_Costa.jpg"
 },
 {
-  "nome": "Pedro Ferreira",
-  "link": "https://media.grupoinveste.pt/gp/person/Pedro_Ferreira.jpg"
-},
-{
-  "nome": "raquel leonardo",
-  "link": "https://media.grupoinveste.pt/gp/person/raquel_leonardo.jpg"
-},
-{
   "nome": "Ricardo Carvalho",
   "link": "https://media.grupoinveste.pt/gp/person/Ricardo_Carvalho.jpg"
 },
-{
-  "nome": "sebastião costa",
-  "link": "https://media.grupoinveste.pt/gp/person/sebastiao_costa.jpg"
-}
 ]
